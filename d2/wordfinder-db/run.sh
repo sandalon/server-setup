d107e17e17344fe5c978c4d6943e355c566b699c
Corey McClelland <corey@wnd.io>
2014-12-28 10:59:13 -0500

    - updates

--- d2/wordfinder-db/run.sh
+++ d2/wordfinder-db/run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-cassandra
+IP='hostname --ip-address'
+
+sed -i -e "s/# JVM_OPTS=\"$JVM_OPTS -Djava.rmi.server.hostname=<public name>\"/ JVM_OPTS=\"$JVM_OPTS -Djava.rmi.server.hostname=$IP\"/" /etc/cassandra/cassandra-env.sh
+
+
+
+cassandra -f
